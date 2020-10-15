800185ab6056ddd47501f7ff69620b4961d3e322
tuandeptr <ttemp5478@gmail.com>
2020-10-15 16:32:06 +0700

    da hoan thanh

--- Source.cpp
+++ Source.cpp
@@ -7,8 +7,16 @@ int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
+	tong(a,b);
+	cout<<"tong la :"<<sum;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 
 	system("pause");
 	return 0;
+}
+int tong(int a ,int b)
+{
+	int sum =0;
+	sum = a+b;
+	return sum;	
 }
